78f467e6e982c9b49e72ea35902062731ef1f18b
same-difference <dnmurillo@gmail.com>
2025-05-01 20:02:58 -0400

    added adapter to config for server hosting friendliness

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter({
+		config: undefined,
+		platformProxy: {
+			configPath: undefined,
+			environment: undefined,
+			persist: undefined
+		},
+		fallback: 'plaintext',
+		routes: {
+			include: ['/*'],
+			exclude: ['<all>']
+		}
+	}) }
 };
 
 export default config;
