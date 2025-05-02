b1894ae66f72c4465757e4e844a3251c983391d8
same-difference <dnmurillo@gmail.com>
2025-05-01 22:57:47 -0400

    landing page ready to go

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-screen bg-violet-950 text-center place-content-center items-center flex flex-row gap-3 md:gap-10">
+<div class="w-1/6 md:w-1/5 text-right text-4xl md:text-8xl">🏗️</div>
+<div class="w-4/6 md:w-3/5 2xl:w-1/5 bg-pink-100/20 rounded-xl p-5">
+    <h1 class="text-2xl md:text-4xl text-violet-200 mb-6 md:mb-10">headsinthe.cloud ☁️ is currently a work in progress</h1>
+    <p class="text-xl md:text-2xl text-violet-200">Come back later when it's done!</p>
+</div>
+<div class="w-1/6 md:w-1/5 text-left text-4xl md:text-8xl">🚧</div>
+</div>
