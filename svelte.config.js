efbcde1c53a8d952dd52bf3301ead68ff0738e06
same-difference <dnmurillo@gmail.com>
2025-05-01 20:23:00 -0400

    debugged svelte config oopsie doopsie

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,17 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter({
-		config: undefined,
-		platformProxy: {
-			configPath: undefined,
-			environment: undefined,
-			persist: undefined
-		},
-		fallback: 'plaintext',
-		routes: {
-			include: ['/*'],
-			exclude: ['<all>']
-		}
-	}) }
+	kit: {
+		adapter: adapter({
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
